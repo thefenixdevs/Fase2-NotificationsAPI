b9ae592dc8870d56b789ab5a4b9df54a57c4b2c5
Adriano <adintelti@gmail.com>
2026-01-13 23:03:04 -0300

    Ajustando fluxo

--- src/Shared/Contracts/Events/PaymentProcessedEvent.cs
+++ src/Shared/Contracts/Events/PaymentProcessedEvent.cs
@@ -5,27 +5,19 @@
         public Guid OrderId { get; private set; }
         public Guid UserId { get; private set; }
         public Guid GameId { get; private set; }
-        public decimal Price { get; private set; }
         public string Status { get; private set; } = string.Empty;
-        public DateTime ProcessedAt { get; private set; }
-
         protected PaymentProcessedEvent() { }
 
         public PaymentProcessedEvent(
             Guid orderId,
             Guid userId,
             Guid gameId,
-            decimal price,
-            string status,
-            DateTime processedAt)
+            string status)
         {
             OrderId = orderId;
             UserId = userId;
             GameId = gameId;
-            Price = price;
             Status = status;
-            ProcessedAt = processedAt;
         }
     }
-
 }
